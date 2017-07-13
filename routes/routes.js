29011e34bf073374840b782c4254553d5d67fa55
rebecca palmore <rebeccapalmore@Rebeccas-MacBook-Pro.local>
2017-07-12 22:56:50 -0500

    adjusting routes

--- routes/routes.js
+++ routes/routes.js
@@ -20,13 +20,28 @@ module.exports = function(app) {
     });
   });
 
-  app.get("/api/members/book/discussion", function(req, res) {
+  // BUT /api/members/book is returning "null" -- not sure why
+  app.get("/api/book", function(req, res) {
+    db.Book.findAll({
+      attributes: ['title']
+    }).then(function(dbBook) {
+        res.json(dbBook);
+    });
+  });
+
+  app.get("/api/book/chapters", function(req, res) {
+    db.Chapter.findAll({}).then(function(dbMember) {
+        res.json(dbMember);
+    });
+  });
+
+  app.get("/api/book/discussion", function(req, res) {
     db.Discussion.findAll({}).then(function(dbMember) {
         res.json(dbMember);
     });
   });
 
-  app.get("/api/members/book/discussion/comments", function(req, res) {
+  app.get("/api/book/discussion/comments", function(req, res) {
     db.Discussion.findAll({
         attributes: ['comment', 'member_name', 'chapter']
     }).then(function(dbMember) {
@@ -34,22 +49,11 @@ module.exports = function(app) {
     });
   });
 
-
-  app.get("/api/members/book", function(req, res) {
-    db.Book.findAll({}).then(function(dbMember) {
-        res.json(dbMember);
-    });
-  });
-
-  app.get("/api/members/join", function(req, res) {
+  // BUT api/members/join is returning "null" -- not sure why
+  app.get("/api/join", function(req, res) {
     db.MemberBook.findAll({}).then(function(dbMember) {
         res.json(dbMember);
     });
   });
 
-  app.get("/api/members/book/chapters", function(req, res) {
-    db.Chapter.findAll({}).then(function(dbMember) {
-        res.json(dbMember);
-    });
-  });
 };
