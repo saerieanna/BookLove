6a9657f77c7336a89d76b085e93a7a81c7620eaa
rebecca palmore <rkpalmore@gmail.com>
2017-07-23 10:50:18 -0500

    updating profile

--- app/components/children/profile.js
+++ app/components/children/profile.js
@@ -7,7 +7,7 @@ var Link = require("react-router").Link;
 
 var helpers = require("../utils/helpers");
 
-import { Image, List, Card, Container, Feed, Progress, Button, Header, Icon, Modal, Dropdown, Menu } from 'semantic-ui-react';
+import { Image, List, Card, Container, Divider, Feed, Progress, Button, Header, Icon, Modal, Dropdown, Menu } from 'semantic-ui-react';
 
 const options = [
               { key: 1, text: 'Chapter 1', value: 1 },
@@ -16,6 +16,19 @@ const options = [
             ]
 
 console.log('OPTIONS ', options[0].key);//returns 1
+
+const divStyle = {
+  padding: 20,
+  backgroundColor: '#80cbc4',
+  backgroundColor: '#b2dfdb',
+};
+
+const listStyle = {
+  padding: 20,
+  marginTop: 20,
+  backgroundColor: '#4db6ac',
+  backgroundColor: '#80cbc4',
+}
 
 var profile = React.createClass({
 
@@ -27,7 +40,8 @@ var profile = React.createClass({
       photo_path: "",
       email: "",
       favorite_book: "",
-      goodreads_url: ""
+      goodreads_url: "",
+      current_book: "",
     };
   },
 
@@ -43,44 +57,39 @@ var profile = React.createClass({
       favorite_book: data.favorite_book,
       goodreads_url: data.goodreads_url,
       current_book: data.current_book,
-      book_title: data.book_title
       });
     }.bind(this));
   },
 
-// We can't display current_book; it's an integer. We need book title instead.
-// <div className="container teal lighten-2">
-
   render: function() {
     return(
-      <div className = "teal lighten-3">
+      <div style={divStyle}>
        <Image src={this.state.photo_path} size='small' shape='circular' centered />
-        <div className="container teal lighten-2">
-        <Modal trigger={<a className="btn btn-floating amber lighten-1 pulse"><i className="material-icons">bookmark_border</i></a>} closeIcon='close'>
-          <Header icon='bookmark' content='What chapter did you just finish?' />
-          <Modal.Content>
-            <Dropdown
-              search
-              searchInput={{ type: 'number' }}
-              selection
-              options={options}
-              placeholder='Select chapter'
-            />
-            <a className="waves-effect waves-light btn amber lighten-1 right"><i className="material-icons left">check</i>Submit</a>
-          </Modal.Content>
-        </Modal>
-        <Progress value='3' total='5' progress='ratio' indicating />
-        <List animated>
-        <List.Item icon='user' content={this.state.first_name + " " + this.state.last_name} />
-          <List.Item icon='book' content={'I am reading ' +this.state.book_title}/>
-          <List.Item icon='dashboard' content="I've read up to chapter 3!" />
-          <List.Item icon='mail' content={this.state.email}/>
-          <List.Item icon='heart' content={'My favorite book: ' +this.state.favorite_book} />
-          <List.Item icon='linkify' content={this.state.goodreads_url}/>
-        </List>
+        <div style={listStyle} className="container">
+          <Modal trigger={<a className="btn btn-floating amber lighten-1 pulse"><i className="material-icons">bookmark_border</i></a>} closeIcon='close'>
+            <Header icon='bookmark' content='What chapter did you just finish?' />
+              <Modal.Content>
+                <Dropdown
+                  search
+                  searchInput={{ type: 'number' }}
+                  selection
+                  options={options}
+                  placeholder='Select chapter'
+                />
+                <a className="waves-effect waves-light btn amber lighten-1 right"><i className="material-icons left">check</i>Submit</a>
+              </Modal.Content>
+          </Modal>
+          <Progress value='3' total='5' progress='ratio' indicating />
+          <List animated>
+            <List.Item icon='user' content={this.state.first_name + " " + this.state.last_name} />
+            <List.Item icon='book' content={'I am reading ' +this.state.current_book}/>
+            <List.Item icon='dashboard' content="I've read up to chapter 3!" />
+            <List.Item icon='mail' content={this.state.email}/>
+            <List.Item icon='heart' content={'My favorite book: ' +this.state.favorite_book} />
+            <List.Item icon='linkify' content={this.state.goodreads_url}/>
+          </List>
         </div>
-      
- </div>
+      </div>
     )
   }
 });
